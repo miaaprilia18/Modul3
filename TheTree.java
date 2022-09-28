90513a97bc46ae5427d880a39dafb633799d8f6b
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2022-09-28 19:46:27 +0700

    Add files via upload

---
+++ TheTree.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul3;
+
+/**
+ *
+ * @author user
+ */
+public class TheTree {
+    public static void main (String[] arg){
+        System.out.println("I think I shall never see,");
+        System.out.println("a poem lovely as a tree.");
+        System.out.println("A tree whose hungry mouth is pressed");
+        System.out.println("Against the Earth's sweet flowing breast.");
+    }
+    
+}
